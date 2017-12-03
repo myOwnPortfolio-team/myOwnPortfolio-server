4624e72fd460395e8e86f7baa5a6843b69710ec2
Thibault Théologien <thibault.theologien@insa-rouen.fr>
2017-12-03 16:03:51 +0100

    UPDATE - first step of portfolio json config generation

--- express.js
+++ express.js
@@ -3,19 +3,19 @@
 const express = require('express');
 const bodyParser = require('body-parser');
 const childProcess = require('child_process');
+const fs = require('fs-extra');
+const slug = require('slug');
 
 const app = express();
 const port = 3000;
+const slugConfig = {
+  lower: true,
+  replacement: '_',
+};
 
-app.use(bodyParser.json());
-
-app.listen(port, () => {
-  console.log(`Server listening on port ${port} !`);
-});
-
-app.post('/portfolio', (req, res) => {
+const runDocker = (res) => {
   childProcess.exec(
-    'docker run macbootglass/myownportfolio-core sleep 5 && echo "done"',
+    'docker run macbootglass/myownportfolio-core sleep 0.5 && echo "done"',
     (error, stdout, stderr) => {
       if (error) res.json({
         status: 0,
@@ -27,4 +27,33 @@ app.post('/portfolio', (req, res) => {
       });
     },
   );
+};
+
+const createModuleConfiguration = (obj, path) => {
+  const formattedModuleName = slug(obj.name, slugConfig);
+  fs.writeJsonSync(`${path}/content/${formattedModuleName}.json`, obj.content);
+  fs.writeJsonSync(`${path}/properties/${formattedModuleName}.json`, obj.properties);
+  fs.writeJsonSync(`${path}/style/${formattedModuleName}.json`, obj.style);
+};
+
+const createPorfolioConfiguration = (conf) => {
+  const path = `./dist/${slug(conf.name, slugConfig)}`;
+  if (fs.existsSync(path)) fs.removeSync(path);
+  fs.mkdirsSync(path);
+  fs.mkdirsSync(`${path}/content`);
+  fs.mkdirsSync(`${path}/properties`);
+  fs.mkdirsSync(`${path}/style`);
+
+  conf.modules.forEach(obj => createModuleConfiguration(obj, path));
+};
+
+app.use(bodyParser.json());
+
+app.listen(port, () => {
+  console.log(`Server listening on port ${port} !`);
+});
+
+app.post('/portfolio', (req, res) => {
+  createPorfolioConfiguration(req.body);
+  runDocker(res);
 });
