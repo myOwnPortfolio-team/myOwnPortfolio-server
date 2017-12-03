d9f72b93c79d135eb43db949debe40a21d9c5cb4
Thibault Théologien <thibault.theologien@insa-rouen.fr>
2017-12-03 16:27:52 +0100

    UPDATE - generation of json config files ok

--- express.js
+++ express.js
@@ -8,6 +8,7 @@ const slug = require('slug');
 
 const app = express();
 const port = 3000;
+const configDir = './dist/config';
 const slugConfig = {
   lower: true,
   replacement: '_',
@@ -29,22 +30,37 @@ const runDocker = (res) => {
   );
 };
 
-const createModuleConfiguration = (obj, path) => {
-  const formattedModuleName = slug(obj.name, slugConfig);
-  fs.writeJsonSync(`${path}/content/${formattedModuleName}.json`, obj.content);
-  fs.writeJsonSync(`${path}/properties/${formattedModuleName}.json`, obj.properties);
-  fs.writeJsonSync(`${path}/style/${formattedModuleName}.json`, obj.style);
+const createModuleConfiguration = (obj, path, moduleName) => {
+  fs.writeJsonSync(`${path}/content/${moduleName}.json`, obj.content);
+  fs.writeJsonSync(`${path}/properties/${moduleName}.json`, obj.properties);
+  fs.writeJsonSync(`${path}/style/${moduleName}.json`, obj.style);
 };
 
 const createPorfolioConfiguration = (conf) => {
-  const path = `./dist/${slug(conf.name, slugConfig)}`;
+  const path = `${configDir}/${slug(conf.name, slugConfig)}`;
   if (fs.existsSync(path)) fs.removeSync(path);
   fs.mkdirsSync(path);
+  fs.writeJsonSync(`${path}/app_properties.json`, conf.app_properties);
   fs.mkdirsSync(`${path}/content`);
   fs.mkdirsSync(`${path}/properties`);
   fs.mkdirsSync(`${path}/style`);
 
-  conf.modules.forEach(obj => createModuleConfiguration(obj, path));
+  const moduleList = [];
+  for (let i = 0; i < conf.modules.length; i++) {
+    const module = conf.modules[i];
+    const formattedModuleName = slug(module.name, slugConfig);
+
+    createModuleConfiguration(module, path, formattedModuleName);
+    moduleList.push({
+      name: module.name,
+      module_path: `./modules/${module.type}/index.jsx`,
+      content_path: `./json_config/content/${formattedModuleName}.json`,
+      properties_path: `./json_config/properties/${formattedModuleName}.json`,
+      style_path: `./json_config/style/${formattedModuleName}.json`,
+      referenced: module.referenced,
+    });
+  }
+  fs.writeJsonSync(`${path}/module_list.json`, moduleList);
 };
 
 app.use(bodyParser.json());
