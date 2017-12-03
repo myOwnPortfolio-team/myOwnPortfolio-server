8b0d0a17fd4b763ee63c0a8eca1a6c46ecc71c4e
Thibault Théologien <thibault.theologien@insa-rouen.fr>
2017-12-03 13:43:09 +0100

    UPDATE - first draft of the config

---
+++ express.js
@@ -0,0 +1,15 @@
+/* eslint no-console: "off" */
+
+const express = require('express');
+// const opener = require('opener');
+
+const app = express();
+const port = 3000;
+
+app.use('/', express.static('dist'));
+
+app.listen(port, () => {
+  console.log(`Server listening on port ${port} !`);
+});
+
+// opener('http://localhost:' + port + '/');
