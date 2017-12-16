29b4f890aa5606483e7ada3eb039d5e153fc6220
Nistof <nistof.contact@gmail.com>
2017-12-16 23:34:30 +0100

    HOTFIX - User id incorrect

--- github/user.js
+++ github/user.js
@@ -3,10 +3,11 @@ const axios = require('axios');
 const getUserID = token => axios
   .get('https://api.github.com/user', {
     headers: {
+      'Content-Type': 'text/json',
       Authorization: `token ${token}`,
     },
   })
-  .then(user => user.id);
+  .then(res => res.data.id);
 
 module.exports = {
   getUserID,
--- portfolio/actions.js
+++ portfolio/actions.js
@@ -1,3 +1,5 @@
+const winston = require('winston');
+
 const express = require('express');
 const fs = require('fs-extra');
 
@@ -9,66 +11,57 @@ const routes = (hostname, port, configDir, webDir, slugConfig) => {
 
   router.post('/portfolio', (req, res) => {
     const config = req.body;
-    const queryParams = req.query;
+    const { token } = req.params;
 
-    if (queryParams.token !== undefined) {
-      const { token } = queryParams;
+    github.getUserID(token)
+      .then((id) => {
+        const path = `${configDir}/${id}`;
 
-      github.getUserID(token)
-        .then((id) => {
-          const path = `${configDir}/${id}`;
+        compile.createPorfolioConfiguration(
+          config,
+          path,
+          slugConfig,
+        );
 
-          compile.createPorfolioConfiguration(
-            config,
-            path,
-            slugConfig,
-          );
-
-          compile.runDocker(
-            res,
-            hostname,
-            port,
-            webDir,
-            configDir,
-            id,
-          );
-        })
-        .catch(err => res.json({
-          status: 400,
-          message: err,
-        }));
-    }
+        compile.runDocker(
+          res,
+          hostname,
+          port,
+          webDir,
+          configDir,
+          id,
+        );
+      })
+      .catch(err => res.json({
+        status: 400,
+        message: err,
+      }));
   });
 
   router.get('/portfolio', (req, res) => {
-    const queryParams = req.query;
+    const { token } = req.query;
 
-    let token;
-    if (queryParams.token !== undefined) {
-      ({ token } = queryParams);
-
-      github.getUserID(token)
-        .then((id) => {
-          const path = `${configDir}/${id}`;
-          fs.readJSONSync(`${path}/saved_config.json`, (err, obj) => {
-            if (err) {
-              res.json({
-                status: 400,
-                message: err,
-              });
-            }
-
+    github.getUserID(token)
+      .then((id) => {
+        const path = `${configDir}/${id}`;
+        fs.readJSONSync(`${path}/saved_config.json`, (err, obj) => {
+          if (err) {
             res.json({
-              status: 200,
-              message: obj,
+              status: 400,
+              message: err,
             });
+          }
+
+          res.json({
+            status: 200,
+            message: obj,
           });
-        })
-        .catch(err => res.json({
-          status: 400,
-          message: err,
-        }));
-    }
+        });
+      })
+      .catch(err => res.json({
+        status: 400,
+        message: err,
+      }));
   });
 
   return router;
