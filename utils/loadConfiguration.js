6830aa5ac3e39e9824aa42f47c06aedf96da59c0
Thibault Théologien <thibault.theologien@insa-rouen.fr>
2017-12-20 14:07:06 +0100

    HOTFIX - path of dist dir

--- utils/loadConfiguration.js
+++ utils/loadConfiguration.js
@@ -8,8 +8,12 @@ module.exports = (path) => {
     configuration.global.dist = process.env.MOP_GLOBAL_DIST;
     configuration.client.id = process.env.MOP_CLIENT_ID;
     configuration.client.secret = process.env.MOP_CLIENT_SECRET;
-  } else configuration.global.dist = `${process.cwd()}/${configuration.global.dist}`;
-  configuration.server.dist.config = `${configuration.global.dist}/${configuration.server.dist.config}`;
-  configuration.server.dist.web = `${configuration.global.dist}/${configuration.server.dist.web}`;
+    configuration.server.dist.config = `/root/dist/${configuration.server.dist.config}`;
+    configuration.server.dist.web = `/root/dist/${configuration.server.dist.web}`;
+  } else {
+    configuration.global.dist = `${process.cwd()}/${configuration.global.dist}`;
+    configuration.server.dist.config = `${configuration.global.dist}/${configuration.server.dist.config}`;
+    configuration.server.dist.web = `${configuration.global.dist}/${configuration.server.dist.web}`;
+  }
   return configuration;
 };
